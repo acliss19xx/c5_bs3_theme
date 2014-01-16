420ec517ea9133f24a1161c7585f567e781ca3e6
acliss19xx <acliss19xx@gmail.com>
2014-01-17 06:28:38 +0900

    add theme files

---
+++ bs303/elements/header.php
@@ -0,0 +1,17 @@
+<?php defined('C5_EXECUTE') or die("Access Denied."); ?>
+
+<!DOCTYPE html>
+<html lang="<?= LANGUAGE ?>">
+
+	<head>
+
+		<?php Loader::element('header_required'); ?>
+
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<!-- Bootstrap -->
+        <link rel="stylesheet" href="<?php echo $this->getThemePath(); ?>/css/bootstrap.min.css" rel="stylesheet">
+        <script src="<?php echo $this->getThemePath(); ?>/js/bootstrap.min.js"></script>
+
+	</head>
+	<body>
+
---
+++ bs303/right_sidebar.php
@@ -0,0 +1,34 @@
+<?php
+defined('C5_EXECUTE') or die("Access Denied.");
+$this->inc('elements/header.php');
+?>
+
+<div class="clear"></div>
+<div class="row">
+
+	<div id="main-content-container" class="col-sm-8">
+		<div id="main-content-inner">
+			<?php
+			$a = new Area('Main');
+			$a->display($c);
+			?>
+
+        </div>
+
+    </div>
+    <div id="left-sidebar-container" class="col-sm-4">
+
+        <div id="left-sidebar-inner">
+
+			<?php
+			$a = new Area('Sidebar');
+			$a->display($c);
+			?>
+
+        </div>
+
+    </div>
+</div>	
+<!-- end main content columns -->
+
+<?php $this->inc('elements/footer.php'); ?>
